ec139ec59e71505178a7fc3d129c83ad3e033086
1saac7 <143825275+1saac7@users.noreply.github.com>
2023-12-24 00:46:04 +0800

    Add head component and update CSS

---
+++ src/lib/components/Head.svelte
@@ -0,0 +1,19 @@
+<svelte:head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="color-scheme" content="dark light" />
+    <meta
+        name="theme-color"
+        content="#232831"
+        media="(prefers-color-scheme: dark)"
+    />
+    <meta
+        name="background-color"
+        content="#232831"
+        media="(prefers-color-scheme: dark)"
+    />
+    <link rel="icon" href="%sveltekit.assets%/icons/favicon.ico" />
+    <link rel="apple-touch-icon" href="%sveltekit.assets%/icons/favicon.png" />
+
+    <link rel="manifest" href="/manifest.webmanifest" />
+</svelte:head>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,12 @@
 <script>
-    import '$lib/github-markdown.css'
+    // import '$lib/github-markdown.css'
+    import '$lib/styles/color.css'
+    import '$lib/styles/main.css'
+    import Head from '$lib/components/Head.svelte'
     import Footer from '$lib/components/Footer.svelte'
 </script>
+
+<Head />
 
 <div class="markdown-body container">
     <main class="main">
@@ -13,7 +18,7 @@
 <style>
     .container {
         margin: 0 auto;
-        padding: 2vh 20px 0 20px;
+        padding: 2vh 30px 0 30px;
         max-width: 768px;
         min-width: 200px;
         display: flex;
